306baa0835b7b6d98df68062b4636b7ef884a250
ggrobelny <ggrobelny@outlook.fr>
2022-01-26 21:12:17 +0100

    ten trzeci

--- not.py
+++ not.py
@@ -12,8 +12,9 @@ x=(screen_width/2) - (width/2)
 y=(screen_height/2) - (height/2)
 root.geometry("%dx%d+%d+%d" % (width,height,x,y))
 root.resizable(0,0)
+SEARCH = StringVar()
 
-#================================================================================
+#==========================================================================================
 
 Top = Frame(root, width=500, bd=1, relief=SOLID)
 Top.pack(side=TOP)
@@ -23,6 +24,23 @@ LeftForm = Frame(MidFrame, width=100)
 LeftForm.pack(side=LEFT)
 RightForm = Frame(MidFrame, width=100)
 RightForm.pack(side=RIGHT)
+
+#==========================================================================================
+
+lbl_title = Label(Top, width=500, font=('arial', 18), text="Python SQLite Search App")
+lbl_title.pack(side=TOP, fill=X)
+lbl_search = Label(LeftForm, font=('arial', 15), text="Search here...")
+lbl_search.pack(side=TOP)
+
+#==========================================================================================
+
+search = Entry(LeftForm, textvariable=SEARCH)
+search.pack(side=TOP, pady=10)
+
+#==========================================================================================
+
+
+
 
 if __name__=='__main__':
     root.mainloop()
