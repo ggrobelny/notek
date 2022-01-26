ccc4b44c572a0caf10eec9585f13bd37932a29eb
ggrobelny <ggrobelny@outlook.fr>
2022-01-26 22:20:29 +0100

    ten czwarty

--- not.py
+++ not.py
@@ -16,6 +16,14 @@ SEARCH = StringVar()
 
 #==========================================================================================
 
+def Search():
+    print("")
+
+def Reset():
+    print("")
+
+#==========================================================================================
+
 Top = Frame(root, width=500, bd=1, relief=SOLID)
 Top.pack(side=TOP)
 MidFrame = Frame(root, width=500)
@@ -27,10 +35,10 @@ RightForm.pack(side=RIGHT)
 
 #==========================================================================================
 
-lbl_title = Label(Top, width=500, font=('arial', 18), text="Python SQLite Search App")
-lbl_title.pack(side=TOP, fill=X)
-lbl_search = Label(LeftForm, font=('arial', 15), text="Search here...")
-lbl_search.pack(side=TOP)
+lblTitle = Label(Top, width=500, font=('arial', 18), text="Python SQLite Search App")
+lblTitle.pack(side=TOP, fill=X)
+lblSearch = Label(LeftForm, font=('arial', 15), text="Search here...")
+lblSearch.pack(side=TOP)
 
 #==========================================================================================
 
@@ -39,7 +47,30 @@ search.pack(side=TOP, pady=10)
 
 #==========================================================================================
 
+btnSearch = Button(LeftForm, text="Search", bg="#006dcc", command=Search)
+btnSearch.pack(side=LEFT)
+btnReset = Button(LeftForm, text="Reset", command=Reset)
+btnReset.pack(side=LEFT)
 
+#==========================================================================================
+
+scrollbarx = Scrollbar(RightForm, orient=HORIZONTAL)
+scrollbary = Scrollbar(RightForm, orient=VERTICAL)
+tree = ttk.Treeview(RightForm, columns=("MemberID", "Title", "Content"), selectmode="extended", height=400, yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
+scrollbary.config(command=tree.yview)
+scrollbary.pack(side=RIGHT, fill=Y)
+scrollbarx.config(command=tree.xview)
+scrollbarx.pack(side=BOTTOM, fill=X)
+
+#==========================================================================================
+
+#==========================================================================================
+
+#==========================================================================================
+
+#==========================================================================================
+
+#==========================================================================================
 
 
 if __name__=='__main__':
