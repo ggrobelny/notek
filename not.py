0734dbe5b7e2394de4b7cea230388fbdec26bfe6
ggrobelny <ggrobelny@outlook.fr>
2022-01-27 22:08:23 +0100

    db, create + insert db btn, search button btn, reset interface and show records btn

--- not.py
+++ not.py
@@ -11,16 +11,53 @@ screen_height=root.winfo_screenheight()
 x=(screen_width/2) - (width/2)
 y=(screen_height/2) - (height/2)
 root.geometry("%dx%d+%d+%d" % (width,height,x,y))
-root.resizable(0,0)
+root.resizable()
 SEARCH = StringVar()
 
 #==========================================================================================
 
+def Database():
+    conn = sqlite3.connect("testowe.db")
+    cursor = conn.cursor()
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS testy (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT) """)
+    cursor.execute("SELECT * FROM testy")
+    if cursor.fetchone() is None:
+        cursor.execute("INSERT INTO testy(title, content) VALUES ('title0', 'content0')")
+        cursor.execute("INSERT INTO testy(title, content) VALUES ('title1', 'content1')")
+        cursor.execute("INSERT INTO testy(title, content) VALUES ('title2', 'content2')")
+        conn.commit()
+
+    cursor.execute("SELECT * FROM testy ORDER BY content ASC")
+    fetch = cursor.fetchall()
+    for data in fetch:
+        tree.insert('', 'end', values=(data))
+    cursor.close()
+    conn.close()        
+    
+
 def Search():
-    print("")
+    if SEARCH.get() != "":
+        tree.delete(*tree.get_children())
+        conn = sqlite3.connect("testowe.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM testy WHERE title LIKE ? OR content LIKE ?", ('%'+str(SEARCH.get())+'%', '%'+str(SEARCH.get())+'%'))
+        fetch = cursor.fetchall()
+        for data in fetch:
+            tree.insert('', 'end', values=(data))
+        cursor.close()
+        conn.close()   
+
 
 def Reset():
-    print("")
+    conn = sqlite3.connect('testowe.db')
+    cursor = conn.cursor()
+    tree.delete(*tree.get_children())
+    cursor.execute("SELECT * FROM testy ORDER BY content ASC")
+    fetch = cursor.fetchall()
+    for data in fetch:
+        tree.insert('', 'end', values=(data))
+    cursor.close()
+    conn.close()
 
 #==========================================================================================
 
@@ -30,12 +67,12 @@ MidFrame = Frame(root, width=500)
 MidFrame.pack(side=TOP)
 LeftForm = Frame(MidFrame, width=100)
 LeftForm.pack(side=LEFT)
-RightForm = Frame(MidFrame, width=100)
+RightForm = Frame(MidFrame, width=500)
 RightForm.pack(side=RIGHT)
 
 #==========================================================================================
 
-lblTitle = Label(Top, width=500, font=('arial', 18), text="Python SQLite Search App")
+lblTitle = Label(Top, width=1250, font=('arial', 18), text="Python SQLite Search App")
 lblTitle.pack(side=TOP, fill=X)
 lblSearch = Label(LeftForm, font=('arial', 15), text="Search here...")
 lblSearch.pack(side=TOP)
@@ -61,6 +98,14 @@ scrollbary.config(command=tree.yview)
 scrollbary.pack(side=RIGHT, fill=Y)
 scrollbarx.config(command=tree.xview)
 scrollbarx.pack(side=BOTTOM, fill=X)
+tree.heading('MemberID', text="MemberID", anchor=W)
+tree.heading('Title', text="Title", anchor=W)
+tree.heading('Content', text="Content")
+tree.column('#0', stretch=NO, minwidth=0, width=0)
+tree.column('#1', stretch=NO, minwidth=0, width=0)
+tree.column('#2', stretch=NO, minwidth=0, width=380)
+tree.column('#3', stretch=NO, minwidth=0, width=700)
+tree.pack()
 
 #==========================================================================================
 
@@ -74,4 +119,5 @@ scrollbarx.pack(side=BOTTOM, fill=X)
 
 
 if __name__=='__main__':
+    Database()
     root.mainloop()
